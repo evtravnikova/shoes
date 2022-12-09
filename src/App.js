f31a3779866289bd32bc893a6af7767da810f076
Olena Travnikova <elena@travnikova.pro>
2022-12-09 20:32:07 +0100

    start header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <header>
+                <div className='headerLeft'>
+                    <img width={40} height={40} src="/img/logo.png" alt="logo"/>
+                    <div className='headerInfo'>
+                        <h3>REACT SNEAKERS</h3>
+                        <p>The best sneakers shop</p>
+                    </div>
+                </div>
+                <ul className='headerRight'>
+                    <li>
+                        <img width={18} height={15} src="/img/cart.svg" alt="cart button"/>
+                        <span>205 zł</span></li>
+                    <li>
+                        <img width={18} height={15} src="/img/fav.svg" alt="favourite button"/>
+                    </li>
+                    <li>
+                        <img width={18} height={15} src="/img/profile.svg" alt="profile button"/>
+                    </li>
+                </ul>
+            </header>
+            <div className="content">
+                <h1>All sneakers</h1>
+            </div>
+        </div>
+    );
 }
 
 export default App;
